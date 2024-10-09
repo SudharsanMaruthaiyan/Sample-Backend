7a10daedcb1b4a7b77fb94f0bd3d32fd75bf0938
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-10-09 14:17:03 +0530

    updated mongo db

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://sudharsan6078:sudharsan123@backend.uoswi.mongodb.net/");
+
+const Schema = new mongoose.Schema({
+    name: String,
+    age : Number
+})
+
+const Sample = mongoose.model("sample",Schema);
+
+app.post("/", async (req,res)=>{
+
+    const newuser = new Sample(req.body);
+
+    try{
+        const saveuser = await newuser.save();
+        res.json({saveuser,message:"Successfully added to db"});
+    }catch(e){
+        res.send("Error in post db")
+    }
+})
+
+
+app.listen(3000);
+
